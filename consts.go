70fae2a438f9f895cdd7ed557677ca5988324d0c
Aamir Maniar <aamir.maniar@maniartech.com>
2021-05-22 23:01:23 +0530

    Better error handling!

---
+++ consts.go
@@ -0,0 +1,13 @@
+package async
+
+// Errors
+
+const errNotABatch = "not-a-batch"
+
+const errInvalidOperation = "invalid-operation"
+
+const errInvalidPromise = "invalid-promise"
+
+const errInvalidState = "invalid-state"
+
+const errInvalidArguments = "invalid-arguments"
