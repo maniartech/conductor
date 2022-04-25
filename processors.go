3405fafbebab227a693cf11533ec3652f98172de
Aamir Maniar <aamir.maniar@maniartech.com>
2022-04-25 11:15:56 +0530

    Promise to future

--- processors.go
+++ processors.go
@@ -5,14 +5,14 @@ import (
 	"sync"
 )
 
-// goConcurrent starts the specified promises in parallel go routines.
-func goConcurrent(p *Promise, args ...interface{}) {
+// goConcurrent starts the specified futures in parallel go routines.
+func goConcurrent(p *Future, args ...interface{}) {
 	wg := sync.WaitGroup{}
 
 	for i := 0; i < len(args); i++ {
-		pr, ok := args[i].(*Promise)
+		pr, ok := args[i].(*Future)
 		if !ok {
-			panic(fmt.Errorf("%s at '%v'", errInvalidPromise, i))
+			panic(fmt.Errorf("%s at '%v'", errInvalidFuture, i))
 		} else if pr.Pending() {
 			panic(fmt.Errorf("%s at '%v'", errInvalidState, i))
 		}
@@ -26,13 +26,13 @@ func goConcurrent(p *Promise, args ...interface{}) {
 	p.Done()
 }
 
-// goQueue starts the specified promises in new go routines with queued mannger.
-// That is it starts the promise only after the preview promise has finished.
-func goQueue(p *Promise, args ...interface{}) {
+// goQueue starts the specified futures in new go routines with queued mannger.
+// That is it starts the future only after the preview future has finished.
+func goQueue(p *Future, args ...interface{}) {
 	for i := 0; i < len(args); i++ {
-		pr, ok := args[i].(*Promise)
+		pr, ok := args[i].(*Future)
 		if !ok {
-			panic(fmt.Errorf("%s at '%v'", errInvalidPromise, i))
+			panic(fmt.Errorf("%s at '%v'", errInvalidFuture, i))
 		} else if pr.Pending() {
 			panic(fmt.Errorf("%s at '%v'", errInvalidState, i))
 		}
@@ -41,27 +41,27 @@ func goQueue(p *Promise, args ...interface{}) {
 	p.Done()
 }
 
-// Create a prmise that executes single handler
-func create(fn PromiseHandler, args ...interface{}) *Promise {
-	return &Promise{
+// Create creates a future task runner that executes single task.
+func create(fn FutureHandler, args ...interface{}) *Future {
+	return &Future{
 		fn:   fn,
 		args: args,
 		wg:   sync.WaitGroup{},
 	}
 }
 
-// Creates a promise that executes one or more handlers
-func createBatch(q bool, promises ...*Promise) *Promise {
-	if len(promises) == 0 {
+// Creates a future that executes one or more handlers
+func createBatch(q bool, futures ...*Future) *Future {
+	if len(futures) == 0 {
 		panic(errInvalidArguments)
 	}
 
-	interfaces := make([]interface{}, len(promises))
-	for i := 0; i < len(promises); i++ {
-		interfaces[i] = promises[i]
+	interfaces := make([]interface{}, len(futures))
+	for i := 0; i < len(futures); i++ {
+		interfaces[i] = futures[i]
 	}
 
-	var p *Promise
+	var p *Future
 
 	if q {
 		p = create(goQueue, interfaces...)
